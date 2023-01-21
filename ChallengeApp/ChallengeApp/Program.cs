db0e091a0f9ac20db2ad6e0bf793544e86dcb17d
Golankiewicz <pgolankiewicz@komster.net>
2023-01-21 16:38:11 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "kobieta";
+int age = 33;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat "+ age);
+}
+
+if(sex=="mężczyzna"&& age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+if(name == "Paweł" && sex=="mężczyzna"&& age == 59)
+{
+    Console.WriteLine("Paweł, szczęśliwy mężczyzna, lat " + age);
+}
